8c1ae7ccd05df30acb484e58045bb8276ebbd865
LeapifyTalk <leapifytalk@gmail.com>
2022-02-28 20:24:28 +0530

    commit

--- LeapifyTalkFront/src/app/Patient/patient-profile/patient-profile.component.ts
+++ LeapifyTalkFront/src/app/Patient/patient-profile/patient-profile.component.ts
@@ -151,18 +151,20 @@ Trial(priceT: any)
   this.cho = false;
 }
 
-selectedata(name : any,CurrentDate : any,WeekDate : any, week : any)
+selectedata(name : any,CurrentDate : any,date : any, week : any)
 {
   console.log('Current Date'+' : '+CurrentDate);
   console.log('Slot'+' : '+name);
-  console.log('Week Date'+' : '+WeekDate);
+  console.log('Week Date'+' : '+date);
   console.log('Week Day'+' : '+week);
   this.cal = false;
   this.comm = true;
 }
 
-commdata()
+commdata(email : any, Link : any)
 {
+  console.log('Doctor Email'+' : '+email);
+  console.log('Communication Link'+' : '+Link);
   this.pay = true;
   this.comm = false; 
 }
@@ -215,70 +217,70 @@ Oncalendar(_id : any, email :any)
     this.WeekDay[i] = this.nextDayValue;
   }
   // =================================================================================
-  // this.ShiftData();
-  for (var j = 0; j < this.WeekDay.length; j++) {
-    // -----------------------------------------------------------------------------
-    if(this.WeekDay[j] == 'Sun')
-      {
-        for (var i = 0; i < this.ResData.Sunday.length; i++) {
-          this.SundayValue = this.ResData.Sunday[i];
-          this.Sunday[i] = this.SundayValue;
-        }
-      // console.log(this.Sunday, 'Sunday Array Elements...............');
-      }
-    // ---------------------------------------------------------------------------
-    if (this.WeekDay[j] == 'Monday') {
-      for (var i = 0; i < this.ResData.Monday.length; i++) {
-        this.MondayValue = this.ResData.Monday[i];
-        this.Monday[i] = this.MondayValue;
-      }
-      // console.log(this.Monday, 'Monday Array Elements...............');
-    }
-    // ----------------------------------------------------------------------------
-    if (this.WeekDay[j] == 'Tuesday') {
-      for (var i = 0; i < this.ResData.Tuesday.length; i++) {
-        this.TuesdayValue = this.ResData.Tuesday[i];
-        this.Tuesday[i] = this.TuesdayValue;
-      }
-      // console.log(this.Tuesday, 'Tuesday Array Elements...............');
-    }
-    // -----------------------------------------------------------------------------
-    if (this.WeekDay[j] == 'Wednesday') {
-      for (var i = 0; i < this.ResData.Wednesday.length; i++) {
-        this.WednesdayValue = this.ResData.Wednesday[i];
-        this.Wednesday[i] = this.WednesdayValue;
-      }
-      // console.log(this.Wednesday, 'Wednesday Array Elements...............');
-    }
-      // ----------------------------------------------------------------------------
-      if(this.WeekDay[j] == 'Thursday')
-      {
-        for (var i = 0; i < this.ResData.Thursday.length; i++) {
-          this.ThursdayValue = this.ResData.Thursday[i];
-          this.Thursday[i] = this.ThursdayValue;
-        }
-        // console.log(this.Thursday, 'Thursday Array Elements...............');
-      }
-      // -----------------------------------------------------------------------------
-      if(this.WeekDay[j] == 'Friday')
-      {
-        for (var i = 0; i < this.ResData.Friday.length; i++) {
-          this.FridayValue = this.ResData.Friday[i];
-          this.Friday[i] = this.FridayValue;
-        }
-        // console.log(this.Friday, 'Friday Array Elements...............');
-      }
-      // -------------------------------------------------------------------------------
-      if(this.WeekDay[j] == 'Saturday')
-      {
-        for (var i = 0; i < this.ResData.Saturday.length; i++) {
-          this.SaturdayValue = this.ResData.Saturday[i];
-          this.Saturday[i] = this.SaturdayValue;
-        }
-      //  console.log(this.Saturday, 'Saturday Array Elements...............');
-      }
-      // ----------------------------------------------------------------------------------
-  }
+  // // this.ShiftData();
+  // for (var j = 0; j < this.WeekDay.length; j++) {
+  //   // -----------------------------------------------------------------------------
+  //   if(this.WeekDay[j] == 'Sunday')
+  //     {
+  //       for (var i = 0; i < this.ResData.Sunday.length; i++) {
+  //         this.SundayValue = this.ResData.Sunday[i];
+  //         this.Sunday[i] = this.SundayValue;
+  //       }
+  //     // console.log(this.Sunday, 'Sunday Array Elements...............');
+  //     }
+  //   // ---------------------------------------------------------------------------
+  //   if (this.WeekDay[j] == 'Monday') {
+  //     for (var i = 0; i < this.ResData.Monday.length; i++) {
+  //       this.MondayValue = this.ResData.Monday[i];
+  //       this.Monday[i] = this.MondayValue;
+  //     }
+  //     // console.log(this.Monday, 'Monday Array Elements...............');
+  //   }
+  //   // ----------------------------------------------------------------------------
+  //   if (this.WeekDay[j] == 'Tuesday') {
+  //     for (var i = 0; i < this.ResData.Tuesday.length; i++) {
+  //       this.TuesdayValue = this.ResData.Tuesday[i];
+  //       this.Tuesday[i] = this.TuesdayValue;
+  //     }
+  //     // console.log(this.Tuesday, 'Tuesday Array Elements...............');
+  //   }
+  //   // -----------------------------------------------------------------------------
+  //   if (this.WeekDay[j] == 'Wednesday') {
+  //     for (var i = 0; i < this.ResData.Wednesday.length; i++) {
+  //       this.WednesdayValue = this.ResData.Wednesday[i];
+  //       this.Wednesday[i] = this.WednesdayValue;
+  //     }
+  //     // console.log(this.Wednesday, 'Wednesday Array Elements...............');
+  //   }
+  //     // ----------------------------------------------------------------------------
+  //     if(this.WeekDay[j] == 'Thursday')
+  //     {
+  //       for (var i = 0; i < this.ResData.Thursday.length; i++) {
+  //         this.ThursdayValue = this.ResData.Thursday[i];
+  //         this.Thursday[i] = this.ThursdayValue;
+  //       }
+  //       // console.log(this.Thursday, 'Thursday Array Elements...............');
+  //     }
+  //     // -----------------------------------------------------------------------------
+  //     if(this.WeekDay[j] == 'Friday')
+  //     {
+  //       for (var i = 0; i < this.ResData.Friday.length; i++) {
+  //         this.FridayValue = this.ResData.Friday[i];
+  //         this.Friday[i] = this.FridayValue;
+  //       }
+  //       // console.log(this.Friday, 'Friday Array Elements...............');
+  //     }
+  //     // -------------------------------------------------------------------------------
+  //     if(this.WeekDay[j] == 'Saturday')
+  //     {
+  //       for (var i = 0; i < this.ResData.Saturday.length; i++) {
+  //         this.SaturdayValue = this.ResData.Saturday[i];
+  //         this.Saturday[i] = this.SaturdayValue;
+  //       }
+  //     //  console.log(this.Saturday, 'Saturday Array Elements...............');
+  //     }
+  //     // ----------------------------------------------------------------------------------
+  // }
   // =================================================================================
 
 }
@@ -332,39 +334,39 @@ BookAppointment(content : any, email :any)
     this.model.open(content, { size: 'xl', backdropClass : 'backdrop-color' });
   // ==============================================================================
 
-  $('fieldset').hide().eq(0).show();
-  $("#finish-step").prop("hidden",true);
+//   $('fieldset').hide().eq(0).show();
+//   $("#finish-step").prop("hidden",true);
   
-  $('#next-step').click(function() {
-      var current = $('fieldset:visible'),
-          next = current.next('fieldset');
-      if (next.length === 0) {
-          next = current.nextUntil('fieldset').next('fieldset');
-      }
-      current.hide();
-      next.show();
-      if (next.nextUntil('fieldset').next('fieldset').add(next.next('fieldset')).length === 0) {
-          $("#next-step").prop("disabled",true);
-          $("#next-step").prop("hidden",true);
-          $("#finish-step").prop("hidden",false);
-      }
-      $("#previous-step").prop("disabled",false);  
-  });
-  $('#previous-step').click(function() {
-      var current = $('fieldset:visible'),
-          prev = current.prev('fieldset');
-      if (prev.length === 0) {
-          prev = current.prevUntil('fieldset').prev('fieldset');
-      }
-      current.hide();
-      prev.show();
-      if (prev.prevUntil('fieldset').prev('fieldset').add(prev.prev('fieldset')).length === 0) {
-          $("#previous-step").prop("disabled",true);
-      }
-      $("#next-step").prop("disabled",false);
-      $("#next-step").prop("hidden",false);
-      $("#finish-step").prop("hidden",true);
-  });
+//   $('#next-step').click(function() {
+//       var current = $('fieldset:visible'),
+//           next = current.next('fieldset');
+//       if (next.length === 0) {
+//           next = current.nextUntil('fieldset').next('fieldset');
+//       }
+//       current.hide();
+//       next.show();
+//       if (next.nextUntil('fieldset').next('fieldset').add(next.next('fieldset')).length === 0) {
+//           $("#next-step").prop("disabled",true);
+//           $("#next-step").prop("hidden",true);
+//           $("#finish-step").prop("hidden",false);
+//       }
+//       $("#previous-step").prop("disabled",false);  
+//   });
+//   $('#previous-step').click(function() {
+//       var current = $('fieldset:visible'),
+//           prev = current.prev('fieldset');
+//       if (prev.length === 0) {
+//           prev = current.prevUntil('fieldset').prev('fieldset');
+//       }
+//       current.hide();
+//       prev.show();
+//       if (prev.prevUntil('fieldset').prev('fieldset').add(prev.prev('fieldset')).length === 0) {
+//           $("#previous-step").prop("disabled",true);
+//       }
+//       $("#next-step").prop("disabled",false);
+//       $("#next-step").prop("hidden",false);
+//       $("#finish-step").prop("hidden",true);
+//   });
 
 });
 
